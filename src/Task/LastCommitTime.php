d49a7b541a8a545629b010d1a185dcad69d0f474
Dima Nagorniy <dima.nagorniy@gmail.com>
2015-12-07 18:08:54 +0200

    Added new task LastCommitTime

---
+++ src/Task/LastCommitTime.php
@@ -0,0 +1,120 @@
+<?php
+//----------------------------------------------------------------------------------------------------------------------
+/**
+ * Class LastCommitTime
+ */
+class LastCommitTime extends Task
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Param for output in console.
+   *
+   * @var bool
+   */
+  private $myVerbose;
+  /**
+   * Build dir.
+   *
+   * @var string
+   */
+  private $myDir;
+  /**
+   * Last commit time.
+   *
+   * @var string
+   */
+  private $myLastCommitTime;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Setter for XML attribute dir.
+   *
+   * @param $theDir
+   */
+  public function setDir($theDir)
+  {
+    $this->myDir = $theDir;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Setter for XML attribute verbose.
+   *
+   * @param $theVerbose
+   */
+  public function setVerbose($theVerbose)
+  {
+    $this->myVerbose = $theVerbose;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Print in console
+   */
+  private function logInfo()
+  {
+    $args   = func_get_args();
+    $format = array_shift($args);
+
+    foreach ($args as &$arg)
+    {
+      if (!is_scalar($arg)) $arg = var_export($arg, true);
+    }
+
+    $this->log(vsprintf($format, $args), Project::MSG_INFO);
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   *  Called by the project to let the task do it's work. This method may be
+   *  called more than once, if the task is invoked more than once. For
+   *  example, if target1 and target2 both depend on target3, then running
+   *  <em>phing target1 target2</em> will run all tasks in target3 twice.
+   *
+   *  Should throw a BuildException if someting goes wrong with the build
+   *
+   *  This is here. Must be overloaded by real tasks.
+   */
+  public function main()
+  {
+    $this->getLastCommitTime();
+
+    $this->setFilesMtime();
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Get last commit time.
+   */
+  private function getLastCommitTime()
+  {
+    $this->myLastCommitTime = exec('git show -s --format=%ct');
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Set last commit time to all files in build directory.
+   */
+  private function setFilesMtime()
+  {
+    $files    = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($this->myDir));
+    foreach ($files as $fullpath => $file)
+    {
+      if ($file->isFile())
+      {
+        if($this->myVerbose)
+        {
+          $this->logInfo("Set new mtime '%s'.",$fullpath);
+        }
+        if (!touch($fullpath,$this->myLastCommitTime)) {
+          throw new \SetBased\Abc\Error\RuntimeException("\nCan't touch file '%s'.\n", $fullpath);
+        }
+      }
+    }
+  }
+  //--------------------------------------------------------------------------------------------------------------------
+
+}
+
+
+//--------------------------------------------------------------------------------------------------------------------
